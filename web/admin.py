57ce207e5ff35218cd94d8b3025596d664f07c67
lover <2197960230@qq.com>
2018-09-13 17:10:23 +0800

    16:59

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from web.models import *
-# Register your models here.
+
 admin.site.register(WebStudent)
 admin.site.register(WebCourse)
 admin.site.register(WebDetail)
