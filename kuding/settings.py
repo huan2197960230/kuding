56995bed93431ac552c99de78f0f4ec6bda3019e
lover <2197960230@qq.com>
2018-09-13 15:54:40 +0800

    urls

--- kuding/settings.py
+++ kuding/settings.py
@@ -65,6 +65,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'strf_time': 'web.templatetags.str_time',
+            }
         },
     },
 ]
